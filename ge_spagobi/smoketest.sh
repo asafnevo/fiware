32476df932960bd3caef81bb7bd3c0b1c635e3a4
julia-nf <j.nieddu-fitremann@beaconinside.com>
2015-10-12 13:13:56 +0200

    add tests description

--- ge_spagobi/smoketest.sh
+++ ge_spagobi/smoketest.sh
@@ -4,45 +4,47 @@ set -e
 
 # Get container id
 container_id=$(docker ps | grep "spagobilabs/spagobi:all-in-one" | awk '{print $1}')
+echo "Container id found : $container_id \n"
 
 # Get IP depending on the OS : IP of the VM on Mac OS, IP of the container on others
-
 if [[ `uname` == 'Darwin'* ]]; then
 	IP=$(docker-machine ip $(docker-machine active))
 else
 	IP=$(docker inspect --format '{{.NetworkSettings.IPAddress}}' ${container_id})
 fi
 
+#Get port
 port_container=$(docker port $container_id)
-echo $port_container
 
 IFS=':' read -a array <<< "$port_container"
 port=$(echo ${array[1]})
 address=$(echo $IP:$port)
 
+echo "Docker-machine test"
 # make sure the virtual machine is on
 if [[ -z $IP ]]; then
-	echo "Docker container not running"
+	echo "Docker container not running\n"
 	exit 1
 else
-	echo "Docker container running at IP : $IP"
+	echo "Docker container found at IP $IP\n"
 fi
 
+echo "Port test"
 if [[ -z $port ]]; then
 	echo "Container's ports not open to the local machine"
 	exit 1
 else
-	echo "Container port : $port"
+	echo "Open port found at $port_container\n"
 fi
 
-echo "\nHomepage test"
+echo "Homepage test"
 status=$(curl -s --head -w %{http_code} $address/SpagoBI -o /dev/null)
 
 if [[ "$status" > '390' ]]; then
 	echo "Homepage not reached. Status : $status"
 	exit 1
 else
-	echo "Homepage found with status : $status"
+	echo "Homepage found with status : $status\n"
 fi
 
-echo "\nSmoketests were successful. \nOK."
+echo "Smoketests OK."
